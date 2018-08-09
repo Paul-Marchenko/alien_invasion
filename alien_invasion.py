ca8bc917be5d07fbf8b94f54819053aebc16d337
Pavel Marchenko <pavel.marchenko@nextiva.com>
2018-08-09 18:15:08 +0300

    changes

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,8 @@ from ship import Ship
 import game_functions as game_f
 from pygame.sprite import Group
 #from alien import Alien
+from game_stats import GameStats
+from button import Button
 
 
 def run_game():
@@ -14,6 +16,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     # pygame.mouse.set_visible(ai_settings.mouse_enabled)
     # pygame.key.get_focused()
     ship = Ship(ai_settings, screen)
@@ -24,14 +29,17 @@ def run_game():
 
 
     while True:
-        game_f.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        game_f.update_bullets(bullets)
-        game_f.update_aliens(aliens)
+        game_f.check_events(ai_settings, screen, stats, sb, play_button, ship,
+           aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            game_f.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            game_f.update_aliens(ai_settings, screen, stats, sb, ship, aliens,
+               bullets)
+            print(len(bullets))
 
-        print(len(bullets))
-
-        game_f.update_screen(ai_settings, screen, ship, aliens, bullets)
+            game_f.update_screen(ai_settings, screen, stats, sb, ship,
+                         aliens, bullets, play_button)
 
 
 run_game()
